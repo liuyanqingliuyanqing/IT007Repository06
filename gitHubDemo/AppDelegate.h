f000a697ce8f862ffefcebf2d42e1e417730dfb0
liuyanqingliuyanqing <1656102217@qq.com>
2016-06-24 15:33:14 +0800

    first
    first

---
+++ gitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHubDemo
+//
+//  Created by mac on 16/6/24.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubDemo
+//
+//  Created by mac on 16/6/24.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
